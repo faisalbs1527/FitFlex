eb6557396d7b339eced05c6ca31ca51a09ad8a66
faisal-ahammed <faisalahammed125@gmail.com>
2024-07-18 11:07:00 +0600

    Dummy data insertion modified

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.core)
     implementation(libs.androidx.runtime.livedata)
+    implementation(libs.androidx.room.common)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
